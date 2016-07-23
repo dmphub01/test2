9a851d04b91206034973432e05b7fd01ec116755
simhadri gokeda <simhadri gokeda>
2016-07-23 17:43:20 +0530

    code

---
+++ scripts/controllers/contact.js
@@ -0,0 +1,16 @@
+
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name DMPHUB.controller:HomeCtrl
+ * @description
+ * # HomeCtrl
+ * Controller of the moreYourApp
+ */
+angular.module('app')
+  .controller('ContactController', function ($scope) {
+    $scope.awesomeThings = [
+      
+    ];
+  });
---
+++ scripts/controllers/homemain.js
@@ -0,0 +1,15 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name DMPHUB.controller:HomeCtrl
+ * @description
+ * # HomeCtrl
+ * Controller of the moreYourApp
+ */
+angular.module('app')
+  .controller('HomemainControl', function ($scope) {
+    $scope.awesomeThings = [
+      
+    ];
+  });
